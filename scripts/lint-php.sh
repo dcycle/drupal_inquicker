f4bcf3c703a71218f352eaff77d5ce74d266826c
Albert Albala <albert@mediatribe.net>
2022-06-08 15:09:06 -0400

    Please squash this commit in the pull request

--- scripts/lint-php.sh
+++ scripts/lint-php.sh
@@ -12,7 +12,7 @@ echo '...'
 echo '// @codingStandardsIgnoreEnd'
 echo ''
 
-docker run --rm -v "$(pwd)"/src:/code dcycle/php-lint:2 \
+docker run --rm -v "$(pwd)"/src:/code dcycle/php-lint:3 \
   --standard=DrupalPractice /code
-docker run --rm -v "$(pwd)"/src:/code dcycle/php-lint:2 \
+docker run --rm -v "$(pwd)"/src:/code dcycle/php-lint:3 \
   --standard=Drupal /code
--- scripts/php-drupal9.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Be ready for Drupal 9.
-#
-set -e
-
-echo "=> Identify deprecated code so we're ready for Drupal 9"
-docker run --rm -v "$(pwd)":/var/www/html/modules/drupal_inquicker dcycle/drupal-check:1 drupal_inquicker
