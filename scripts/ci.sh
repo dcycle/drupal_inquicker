e59d1e464508694cfd62fb1ab1ace7c7bde183ba
Albert Albala <albert@mediatribe.net>
2022-06-09 09:22:58 -0400

    Drop Drupal 8 support (#8)

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -9,33 +9,9 @@ echo " => Running fast tests"
 echo ""
 ./scripts/test.sh
 echo ""
-echo " => Making sure we have no Drupal-9-incompatible code"
-echo ""
-./scripts/php-drupal9.sh
-echo ""
-echo " => Deploying Drupal 8"
-echo ""
-./scripts/deploy.sh
-echo ""
-echo " => Running unit tests on Drupal 8"
-echo ""
-./scripts/php-unit-drupal.sh
-echo ""
-echo " => Running some tests on the running Drupal 8 environment"
-echo ""
-./scripts/test-running-environment.sh
-echo ""
-echo " => Destroying Drupal 8"
-echo ""
-docker-compose down -v
-echo ""
 echo " => Deploying Drupal 9"
 echo ""
 ./scripts/deploy.sh 9
-echo ""
-echo " => Running unit tests on Drupal 9"
-echo ""
-./scripts/php-unit-drupal.sh
 echo ""
 echo " => Running some tests on the running Drupal 8 environment"
 echo ""
