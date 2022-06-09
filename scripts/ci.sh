68183836cdf7715b29bbb4cf0a511471e11fff7a
Albert Albala <albert@mediatribe.net>
2022-06-08 22:39:06 -0400

    Please squash this commit in the pull request

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -13,10 +13,6 @@ echo " => Deploying Drupal 9"
 echo ""
 ./scripts/deploy.sh 9
 echo ""
-echo " => Running unit tests on Drupal 9"
-echo ""
-./scripts/php-unit-drupal.sh
-echo ""
 echo " => Running some tests on the running Drupal 8 environment"
 echo ""
 ./scripts/test-running-environment.sh
